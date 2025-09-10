78060a710a7765874e292bada7b0cf9f886b217c
Noah <marcel.w@friendlyhost.email>
2025-09-10 11:35:03 +0200

    updated 2 files

---
+++ src/AusiDevelopmentPM/HoloAPI/HoloAPI.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AusiDevelopmentPM\HoloAPI;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\SingletonTrait;
+
+class HoloAPI extends PluginBase {
+
+    use SingletonTrait;
+
+    public function onEnable() :void {
+        self::setInstance($this);
+    }
+
+}
