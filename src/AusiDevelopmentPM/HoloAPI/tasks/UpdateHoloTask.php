7a3240e078854305873cbead3ec6ab4425b216ac
Noah <marcel.w@friendlyhost.email>
2025-09-10 11:42:52 +0200

    Update UpdateHoloTask.php

--- src/AusiDevelopmentPM/HoloAPI/tasks/UpdateHoloTask.php
+++ src/AusiDevelopmentPM/HoloAPI/tasks/UpdateHoloTask.php
@@ -4,13 +4,21 @@ declare(strict_types=1);
 
 namespace AusiDevelopmentPM\HoloAPI\tasks;
 
+use AusiDevelopmentPM\HoloAPI\HoloAPI;
 use pocketmine\scheduler\Task;
 
 class UpdateHoloTask extends Task {
 
+    public function __construct(HoloAPI $API)
+    {
+
+    }
 
     public function onRun(): void
     {
-        // TODO: Implement onRun() method.
+        $holos = HoloAPI::getInstance()->getHolos();
+        foreach ($holos as $holo => $holoData) {
+            $holoData->update();
+        }
     }
 }
